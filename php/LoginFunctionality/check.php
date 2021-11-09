c82f6bf2b5b14f7f469f953f89748b19218d9e45
shyamkumbhar <shyamkumbhar509@gmail.com>
2021-11-09 14:12:09 +0530

    LOgin Functinality

--- php/LoginFunctionality/check.php
+++ php/LoginFunctionality/check.php
@@ -1,22 +1,39 @@
 <?php
+$email = $_POST['email'];
+$password = $_POST['password'];
 
-extract ($_POST);
-if(isset($loginSubmit)){
-    /* mysql connectivity */
-$con = new mysqli("localhost", "root", "", "reg_10am");
 
-/* write Query */
-$query = "SELECT * FROM users WHERE EMAIL = '$uemail'  ";
+/* step-1 Db Connection */
+$con = new mysqli('localhost', 'root', '', 'proj');
+
+/* step-2 check Database Connection */
+if ($con->client_info) {
+    // echo "Database is Connected <br>";
+} else {
+    echo "Database is Fail..<br>";
+}
+
+/* step3- Write Query */
+$sql = "SELECT * FROM users WHERE email='$email' AND password='$password'";
+
+
+/* Step-4 Quey Exicute/query fire */
+$result = $con->query($sql);
+
+
+/* Step-5 fetch data/Output */
+// $data = $result->fetch_object();
+// print_r($result);
+
+if ($result->num_rows ==1) {
+    // echo "Valied User .<br>";
+    header("location:welcome.php");
+
+} else {
+    echo "InValied User .<br>";
+
 }
 
 
+
 ?>
-
-
-
-<!-- Array  // FOR DEVLOPMENT PROCESS
-(
-    [uemail] => 
-    [upassword] => 
-    [loginSubmit] => 
-) -->
--- php/index.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-echo "string";
-echo "<h1>Hello world</h1>";
-
-
-
- ?>
